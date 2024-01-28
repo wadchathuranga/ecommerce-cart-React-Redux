4b7fe5d292abb27e067fd6192be2266826fd0670
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-01-28 14:17:15 +0530

    redux setuped

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { allReducers } from './reducers';
+
+
+const store = configureStore({
+    reducer: allReducers,
+});
+
+
+export default store;
