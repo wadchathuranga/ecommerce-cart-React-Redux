87eddedf52802af1cafd12d577809080580e650e
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-01-28 20:42:49 +0530

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav className="bg-gray-100">
+      <div className="w-full h-20 px-10">
+        <div className="flex justify-between">
+          <div>
+            <a href="" className="flex space-x-3">
+              <img
+                className="h-8 w-auto"
+                src="src/assets/react.svg"
+                alt="Your Company"
+              />
+              <span>Redux-CRUD</span>
+            </a>
+          </div>
+
+          <div className="flex space-x-3">
+            <Link to={"/home"}>Home</Link>
+            <Link to={"/products"}>Products</Link>
+            <Link to={"/about"}>About</Link>
+            <Link to={"/contact"}>Contact</Link>
+          </div>
+
+          <div>Sencondary Nav</div>
+
+          {/* Mobile Nav */}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
