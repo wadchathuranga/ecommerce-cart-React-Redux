1e4166cfc3d950f6c6823a8ca3665734c53b02f1
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-01-29 22:11:59 +0530

    routes changed

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,67 +1,88 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 import App from "../App.jsx";
 import Home from "../pages/Home.jsx";
 import Contact from "../pages/Contact.jsx";
 import About from "../pages/About.jsx";
 import Products from "../pages/Products.jsx";
+import Profile from "../pages/Profile.jsx";
 import Login from "../pages/Login.jsx";
 import Signup from "../pages/Signup.jsx";
 import NotFound from "../pages/NotFound.jsx";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        path: "/not-found",
-        element: <NotFound />,
-      },
-      {
-        path: "/login",
-        element: (
-          // <Protected authentication={false}>
-          <Login />
-          // </Protected>
-        ),
-      },
-      {
-        path: "/signup",
-        element: (
-          // <Protected authentication={false}>
-          <Signup />
-          // </Protected>
-        ),
-      },
-      {
-        path: "/products",
-        element: <Products />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      // {
-      //   path: "/users/profile/me",
-      //   element: (
-      //     <Protected authentication>
-      //       <MyAccount />
-      //     </Protected>
-      //   ),
-      // },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/not-found" element={<NotFound />} />
+    </Route>
+  )
+);
+
+// export const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/home",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/not-found",
+//         element: <NotFound />,
+//       },
+//       {
+//         path: "/login",
+//         element: (
+//           // <Protected authentication={false}>
+//           <Login />
+//           // </Protected>
+//         ),
+//       },
+//       {
+//         path: "/signup",
+//         element: (
+//           // <Protected authentication={false}>
+//           <Signup />
+//           // </Protected>
+//         ),
+//       },
+//       {
+//         path: "/products",
+//         element: <Products />,
+//       },
+//       {
+//         path: "/about",
+//         element: <About />,
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />,
+//       },
+//       // {
+//       //   path: "/users/profile/me",
+//       //   element: (
+//       //     <Protected authentication>
+//       //       <MyAccount />
+//       //     </Protected>
+//       //   ),
+//       // },
+//     ],
+//   },
+// ]);
