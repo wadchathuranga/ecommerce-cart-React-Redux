18e62da4a829e20d09981d591b63cf7e206037ef
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-01-29 22:16:26 +0530

    products api slice injected

---
+++ src/store/api/productsApiSlice.js
@@ -0,0 +1,11 @@
+import { apiSlice } from "./apiSlice";
+
+const API = '';
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        //
+    })
+});
+
+export const { } = usersApiSlice; 
