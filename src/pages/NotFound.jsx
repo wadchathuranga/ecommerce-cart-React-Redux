94d693a0adbbcb80b4b2e269c941986294bcf199
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-01-29 22:12:29 +0530

    not found page added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <p className="font-bold">404 | Page Not Found!</p>
+      <p className="font-bold text-3xl">404 | Page Not Found!</p>
     </div>
   );
 };
