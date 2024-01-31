7b3409f5d4f746384c62c5ec102c5d6a89f8fce7
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-01-31 19:16:54 +0530

    temp commit

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -18,10 +18,13 @@ const ItemCard = ({ item }) => {
         <img className="w-32 h-32" src={item.image} alt="productImage" />
         <h1 className="font-titleFont font-semibold">{item.title}</h1>
       </div>
+
       <div className="col-span-5 mdl:col-span-3 flex items-center justify-between py-4 mdl:py-0 px-4 mdl:px-0 gap-6 mdl:gap-0">
+        {/* price */}
         <div className="flex w-1/3 items-center text-lg font-semibold">
           ${item.price}
         </div>
+        {/* quantity */}
         <div className="w-1/3 flex items-center gap-6 text-lg">
           <span
             onClick={() => dispatch(drecreaseQuantity(item.id))}
@@ -37,6 +40,7 @@ const ItemCard = ({ item }) => {
             +
           </span>
         </div>
+        {/* total price */}
         <div className="w-1/3 flex items-center font-titleFont font-bold text-lg">
           <p>${item.quantity * item.price}</p>
         </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = () => {
     <div className="max-w-container mx-auto px-4">
       {cartItems.length > 0 ? (
         <div className="pb-20">
-          <div className="w-full h-20 bg-[#F5F7F7] text-primeColor hidden lgl:grid grid-cols-5 place-content-center px-6 text-lg font-titleFont font-semibold">
+          <div className="w-full h-20 bg-[#F5F7F7] text-primeColor lgl:grid grid-cols-5 place-content-center px-6 text-lg font-titleFont font-semibold">
             <h2 className="col-span-2">Product</h2>
             <h2>Price</h2>
             <h2>Quantity</h2>
