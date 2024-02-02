3ff48e4181923c0f4f38b58e2ccb0f8979792030
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-02 22:11:00 +0530

    item card further improve design

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -9,40 +9,24 @@ import {
 const ItemCard = ({ item }) => {
   const dispatch = useDispatch();
   return (
-    <div className="w-full grid grid-cols-5 mb-4 border py-2">
-      <div className="flex col-span-5 mdl:col-span-2 items-center gap-4 ml-4">
+    <div className="w-full grid grid-rows-2 grid-flow-col mb-4 py-3 border">
+      <div className="row-span-2 flex items-center justify-evenly">
         <ImCross
           onClick={() => dispatch(deleteItem(item.id))}
           className="text-primeColor hover:text-red-500 duration-300 cursor-pointer"
         />
         <img className="w-32 h-32" src={item.image} alt="productImage" />
-        <h1 className="font-titleFont font-semibold">{item.title}</h1>
       </div>
 
-      <div className="col-span-5 mdl:col-span-3 flex items-center justify-between py-4 mdl:py-0 px-4 mdl:px-0 gap-6 mdl:gap-0">
-        {/* price */}
-        <div className="flex w-1/3 items-center text-lg font-semibold">
-          ${item.price}
+      <div className="bg-green-500 row-span-2 col-span-6">
+        <div className="bg-slate-400 h-2/4 flex items-center pl-5">
+          <h1 className="font-titleFont font-semibold">{item.title}</h1>
         </div>
-        {/* quantity */}
-        <div className="w-1/3 flex items-center gap-6 text-lg">
-          <span
-            onClick={() => dispatch(drecreaseQuantity(item.id))}
-            className="w-6 h-6 bg-gray-100 text-2xl flex items-center justify-center hover:bg-gray-300 cursor-pointer duration-300 border-[1px] border-gray-300 hover:border-gray-300"
-          >
-            -
-          </span>
-          <p>{item.quantity}</p>
-          <span
-            onClick={() => dispatch(increaseQuantity(item.id))}
-            className="w-6 h-6 bg-gray-100 text-2xl flex items-center justify-center hover:bg-gray-300 cursor-pointer duration-300 border-[1px] border-gray-300 hover:border-gray-300"
-          >
-            +
-          </span>
-        </div>
-        {/* total price */}
-        <div className="w-1/3 flex items-center font-titleFont font-bold text-lg">
-          <p>${item.quantity * item.price}</p>
+
+        <div className="bg-yellow-400 h-2/4 flex items-center justify-around">
+          <div className="bg-red-500">1</div>
+          <div className="bg-red-400">2</div>
+          <div className="bg-red-300">3</div>
         </div>
       </div>
     </div>
