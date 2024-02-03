7997ae6dfbfddc16740713d5d2d0b68a15ffb1e6
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-02-03 13:19:10 +0530

    cart page improve

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,11 +10,16 @@ const Cart = () => {
     <div className="max-w-container mx-auto px-4">
       {cartItems.length > 0 ? (
         <div className="pb-20">
-          <div className="w-full h-20 bg-[#F5F7F7] text-primeColor lgl:grid grid-cols-5 place-content-center px-6 text-lg font-titleFont font-semibold">
-            <h2 className="col-span-2">Product</h2>
-            <h2>Price</h2>
-            <h2>Quantity</h2>
-            <h2>Sub Total</h2>
+          <div className="w-full h-20 bg-[#F5F7F7] grid grid-flow-col items-center text-primeColor px-10 text-lg font-titleFont font-semibold">
+            <div className="space-x-10">
+              <h2 className="">Product</h2>
+            </div>
+
+            <div className="flex items-center justify-end space-x-28">
+              <h2>Price</h2>
+              <h2>Quantity</h2>
+              <h2>Sub Total</h2>
+            </div>
           </div>
           <div className="mt-5">
             {cartItems.map((item) => (
@@ -31,42 +36,53 @@ const Cart = () => {
             Reset cart
           </button>
 
-          <div className="flex flex-col mdl:flex-row justify-between border py-4 px-4 items-center gap-2 mdl:gap-0">
-            <div className="flex items-center gap-4">
+          <div className="flex items-center justify-end border py-4 px-4 ">
+            <div className="flex items-center gap-3">
               <input
                 className="w-44 mdl:w-52 h-8 px-4 border text-primeColor text-sm outline-none border-gray-400"
                 type="text"
                 placeholder="Coupon Number"
               />
-              <p className="text-sm mdl:text-base font-semibold">
+              <button
+                onClick={() => dispatch(clearCart())}
+                className="text-sm font-semibold bg-black text-white duration-100 py-2 px-2"
+              >
                 Apply Coupon
-              </p>
+              </button>
             </div>
-            <p className="text-lg font-semibold">Update Cart</p>
           </div>
-          <div className="max-w-7xl gap-4 flex justify-end mt-4">
+
+          <div className=" gap-4 flex justify-end mt-4">
             <div className="w-96 flex flex-col gap-4">
               <h1 className="text-2xl font-semibold text-right">Cart totals</h1>
+
               <div>
                 <p className="flex items-center justify-between border-[1px] border-gray-400 border-b-0 py-1.5 text-lg px-4 font-medium">
                   Subtotal
                   <span className="font-semibold tracking-wide font-titleFont">
-                    $999
+                    $ {Number(10 + 20).toFixed(2)}
                   </span>
                 </p>
                 <p className="flex items-center justify-between border-[1px] border-gray-400 border-b-0 py-1.5 text-lg px-4 font-medium">
                   Shipping Charge
                   <span className="font-semibold tracking-wide font-titleFont">
-                    $30
+                    $ {Number(8 + 9).toFixed(2)}
+                  </span>
+                </p>
+                <p className="flex items-center justify-between border-[1px] border-gray-400 border-b-0 py-1.5 text-lg px-4 font-medium">
+                  Coupon Discount
+                  <span className="font-semibold tracking-wide font-titleFont">
+                    - $ {Number(3 + 2).toFixed(2)}
                   </span>
                 </p>
                 <p className="flex items-center justify-between border-[1px] border-gray-400 py-1.5 text-lg px-4 font-medium">
                   Total
                   <span className="font-bold tracking-wide text-lg font-titleFont">
-                    ${10 + 18}
+                    $ {Number(47 - 5).toFixed(2)}
                   </span>
                 </p>
               </div>
+
               <div className="flex justify-end">
                 <Link to="/paymentgateway">
                   <button className="w-52 h-10 bg-primeColor text-white hover:bg-black duration-300">
